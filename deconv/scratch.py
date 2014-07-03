9e1dc8f92a3932721a19d031d7ca6d4623a5319b
Charles Yang Zheng <snarles@celadon>
2014-07-03 14:44:51 -0700

    starting up branch

---
+++ deconv/scratch.py
@@ -0,0 +1,22 @@
+# Just a script to test things out
+
+import numpy as np
+import scipy as sp
+
+def fullfact(levels)
+    """ Creates a full factorial design matrix
+
+    Parameters
+    ----------
+    levels : list of integers specifying number of levels of each factor
+    """
+
+def sph_lattice(resolution, radius)
+    """ Creates a N x 3 array of points *inside* a sphere
+
+    Parameters
+    ----------
+    resolution: determines the minimum distance between points
+      as radius/resolution
+    radius: radius of the sphere
+    """
