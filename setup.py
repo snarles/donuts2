31ee1805569bd3a5fdbfaf5959404efae42fd938
arokem <arokem@gmail.com>
2014-07-02 17:21:59 -0700

    RF: Created module directory structure. Added setup file for installation.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+
+
+EXTENSIONS = [dict(name="donuts.emd",
+                   sources=["donuts/emd/pyemd.c", "donuts/emd/emd.c"],
+                   extra_compile_args=['-g'])]
+
+opts = dict(name='donuts',
+            packages=['donuts',
+                      'donuts.emd'],
+            ext_modules = [Extension(**e) for e in EXTENSIONS])
+
+if __name__ == '__main__':
+    setup(**opts)
