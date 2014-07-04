d110a5ccc65b71be1af310be146239b1cd88fefc
Charles Yang Zheng <snarles@celadon>
2014-07-03 17:05:00 -0700

    simulation stuff is in one file

--- deconv/scratch.py
+++ deconv/scratch.py
@@ -28,6 +28,15 @@ def norms(x) :
     nms = np.sum(np.abs(x)**2,axis=-1)**(1./2)
     return nms
 
+def normalize_rows(x):
+    # normalizes the rows of x
+    n = np.shape(x)[0]
+    p = np.shape(x)[1]
+    nms = norms(x).reshape(-1,1)
+    x = np.multiply(x, 1./np.tile(nms,(1,p)))
+    return x
+    
+
 def sph_lattice(resolution, radius):
     """ Creates a ? x 3 array of points *inside* a sphere
     
@@ -65,9 +74,44 @@ def ste_tan_kappa(grid, bvecs):
     -------
     x : N x M numpy array, columns are ST kernel signals
     """
+    x = np.exp(-np.dot(grid, bvecs.T)**2).T
+    return x
 
+def simulate_signal_kappa(fibers, weights, bvecs, sigma):
+    """ Simulates (Rician) noisy and noiseless signal from a voxel
+        with fiber directions specified by fibers,
+        fiber kappas specified by root-norm of fibers,
+        noise scaling specified by sigma, and 
+        measurement directions bvecs
+    
+    Parameters
+    ----------
+    fibers: K x 3 numpy array of fiber directions with length
+      equal to square root of kappa
+    weights: K x 1 numpy array of fiber weights
+    bvecs: N x 3 numpy array of unit vectors
+      corresponding to DWI measurement directions
+    
+    Returns
+    -------
+    signals : 2-length list
+    signals[0] : N x 1 numpy array, noiseless signal
+    signals[1] : N x 1 numpy array, noisy signal
+    """
+    x = ste_tan_kappa(fibers, bvecs)
+    n = np.shape(bvecs)[0]
+    y0 = np.dot(x,weights)
+    raw_err = sigma*np.random.normal(0,1,(n,2))
+    raw_err[:,0] = raw_err[:,0] + np.squeeze(y0)
+    y1 = norms(raw_err).reshape(-1,1)
+    return [y0,y1]
 
 grid = sph_lattice(3,2)
-bvecs = np.random.normal(0,1,(100,3))
-nms = norms(bvecs)
-bvecs = np.multiply(bvecs,1/np.tile(nms.reshape(-1,1), (1,3)))
+bvecs = normalize_rows(np.random.normal(0,1,(100,3)))
+x = ste_tan_kappa(grid,bvecs)
+
+true_kappa = 1.5
+true_pos = normalize_rows(np.random.normal(0,1,(3,3)))
+true_w = np.array([1,1,1]).reshape((-1,1))
+res = simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,0.1)
+
