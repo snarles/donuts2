58e1ca642b210b19bbada32102ea3b8470b272ff
Charles Yang Zheng <snarles@celadon>
2014-07-05 21:05:26 -0700

    started writing tests

--- donuts/tests/test_deconv.py
+++ donuts/tests/test_deconv.py
@@ -2,30 +2,33 @@ import numpy as np
 import scipy as sp
 import scipy.optimize as spo
 import donuts.deconv.utils as du
+import numpy.testing as npt
+
 
 import dipy.data as dpd
 s1 = dpd.get_sphere('symmetric362')
 s2 = s1.subdivide() # s2 has 1442 vertices
-
+bvecs = s1.vertices
+grid = s2.vertices
 
 def test_fullfact():
     a = du.fullfact([3,3,3])
+    npt.assert_almost_equal(np.size(a),27)
 
+def test_normalize_rows():
+    a = du.normalize_rows(np.random.normal(0,1,(10000,3)))
+    npt.assert_almost_equal(sum(a[1,]**2),1)
 
-def blah():
-    # setup bvecs and grid; using random points for now, but to be replaced by other code
-    grid = du.normalize_rows(np.random.normal(0,1,(10000,3)))
-    bvecs = du.normalize_rows(np.random.normal(0,1,(n,3)))
+def test_simulate_signal_kappa():
     true_kappa = 1.5
-    true_pos = normalize_rows(np.random.normal(0,1,(3,3)))
+    true_pos = bvecs[[1,10,11],]
     true_w = np.array([1,1,1]).reshape((-1,1))
-    res = simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,0.1)
+    res = du.simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,0.1)
     y0=res[0]
     y1=res[1]
-
     # test if NNLS recovers the correct positions for noiseless data
     kappa=1.5
-    xs = ste_tan_kappa(np.sqrt(kappa)*grid,bvecs)
+    xs = du.ste_tan_kappa(np.sqrt(kappa)*grid,bvecs)
     beta = spo.nnls(xs,np.squeeze(y0))[0]
     est_pos = grid[np.nonzero(beta),:]
     est_w = beta[np.nonzero(beta)]
