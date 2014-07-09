039c187257e4f4c0a16b38b97583b0832df32e08
arokem <arokem@gmail.com>
2014-07-09 14:05:50 -0700

    TST : more testing of EMD.

--- donuts/tests/test_emd.py
+++ donuts/tests/test_emd.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.testing as npt
+import scipy.spatial.distance as dist
 
 import donuts.emd as emd
 
@@ -12,6 +13,12 @@ def test_emd():
         for this_y in xx:
             cost.append(np.sqrt(sum((this_x - this_y)**2)))
 
-    ee = emd.emd([0, 1, 0], [1, 0, 0], list(cost))
+    ee = emd.emd([0, 1, 0], [1, 0, 0], cost)
     npt.assert_almost_equal(ee, np.sqrt(2), decimal=5)
 
+    # What about different locations? 
+    xx =  np.array([[1,0,0], [0,1,0], [0,0,1]])
+    yy = np.array([[1,0,0], [0,1,0], [0,0,1], [np.sqrt(2), np.sqrt(2), 0]])
+    cost = dist.cdist(xx, yy).ravel()
+    ee = emd.emd([0, 1, 0], [1, 0, 0, 0], cost)
+    npt.assert_almost_equal(ee, np.sqrt(2), decimal=5)
