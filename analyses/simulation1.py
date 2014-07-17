9161458b3892c574e2c17c655cb77ddd016817a1
Charles Yang Zheng <snarles@celadon>
2014-07-17 09:25:51 -0700

    sim1

--- analyses/simulation1.py
+++ analyses/simulation1.py
@@ -1,45 +1,31 @@
 import numpy as np
 import donuts.deconv.utils as du
 import dipy.data as dpd
+import donuts.data as dnd
 s1 = dpd.get_sphere('symmetric362')
 s2 = s1.subdivide() # s2 has 1442 vertices
-bvecs = s1.vertices
 grid = s2.vertices
+kappas = np.arange(1.5,4,.1)
+
+data, bvecs0, bvals = dnd.load_hcp_cso()
+bvecs0=bvecs0.T
+bval = 3000
+idx = np.squeeze(np.nonzero(np.logical_and(bvals > bval-20, bvals < bval+20)))
+bvecs = bvecs0[idx,:]
+xss = du.build_xss(grid,bvecs,kappas)
 n= np.shape(bvecs)[0]
 
+# simulation
 true_k = 3
 true_pos = du.normalize_rows(np.random.normal(0,1,(true_k,3)))
 true_w = np.ones((true_k,1))
 true_kappa = 2
 true_sigma = 0.2
 y0, y1 = du.simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,true_sigma)
+sel_kappa, cves = du.cv_sel_params(y1,xss,20,kappas)
 
+# real data
 kappas = np.arange(1.5,4,.1)
-xss = [0]*len(kappas)
-for i in range(len(kappas)):
-    kappa = kappas[i]
-    xss[i] = du.ste_tan_kappa(np.sqrt(kappa)*grid,bvecs)
-cves = [0.]*len(kappas)
-for i in range(len(kappas)):
-    kappa = kappas[i]
-    cves[i] = sum(du.cv_nnls(y1,xss[i],20))
-sel_kappa = kappas[du.rank_simple(cves)[0]]
+sel_kappa, cves = du.cv_sel_params(data[5,3,0,idx],xss,20,kappas)
+sel_kappa
 
-
-import donuts.data as dnd
-data, bvecs0, bvals = dnd.load_hcp_cso()
-bvecs0=bvecs0.T
-idx = np.squeeze(np.nonzero(np.logical_and(bvals > 980, bvals < 1020)))
-
-
-y = data[2,3,0,:]
-kappas = np.arange(.5,4,.1)
-xss = [0]*len(kappas)
-for i in range(len(kappas)):
-    kappa = kappas[i]
-    xss[i] = du.ste_tan_kappa(np.sqrt(kappa)*grid,bvecs)
-cves = [0.]*len(kappas)
-for i in range(len(kappas)):
-    kappa = kappas[i]
-    cves[i] = sum(du.cv_nnls(y,xss[i],20))
-sel_kappa = kappas[du.rank_simple(cves)[0]]
