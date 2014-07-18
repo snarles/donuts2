578b822099253399f33e476aedc9504323d51334
Charles Yang Zheng <snarles@celadon>
2014-07-17 17:30:42 -0700

    sim 2 multiple voxels pooled

--- analyses/simulation2.py
+++ analyses/simulation2.py
@@ -9,7 +9,7 @@ kappas = np.arange(0.5,4,.1)
 
 data, bvecs0, bvals = dnd.load_hcp_cso()
 bvecs0=bvecs0.T
-bval = 2000
+bval = 3000
 idx = np.squeeze(np.nonzero(np.logical_and(bvals > bval-20, bvals < bval+20)))
 bvecs = bvecs0[idx,:]
 xss = du.build_xss(grid,bvecs,kappas)
@@ -19,18 +19,27 @@ n= np.shape(bvecs)[0]
 nits = 10
 all_cves = np.zeros((len(kappas),nits))
 for ii in range(nits):
-    true_k = 1
+    true_k = 3
     true_pos = du.normalize_rows(np.random.normal(0,1,(true_k,3)))
     true_w = np.ones((true_k,1))/true_k
     true_kappa = 2
-    true_sigma = 0.5
+    true_sigma = 0.1
     y0, y1 = du.simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,true_sigma)
     sel_kappa, cves = du.cv_sel_params(y1,xss,10,kappas)
     all_cves[:,ii] = cves
+
 sum_cves = np.sum(all_cves,axis=1)
 sel_kappa = kappas[du.rank_simple(sum_cves)[0]]
 
 # real data
-sel_kappa, cves = du.cv_sel_params(data[5,3,0,idx],xss,20,kappas)
-sel_kappa
+
+sel_inds = [[5,5],[5,15],[5,25],[5,35],[15,5],[15,15],[15,25],[15,35],[25,5],[25,15],[25,25],[25,35],[35,5],[35,15],[35,25],[35,35]]
+all_cves = np.zeros((len(kappas),len(sel_inds)))
+for ii in range(len(sel_inds)):
+    sel_kappa, cves = du.cv_sel_params(data[sel_inds[ii][0],sel_inds[ii][1],0,idx],xss,20,kappas)
+    all_cves[:,ii] = cves
+
+sum_cves = np.sum(all_cves,axis=1)
+sel_kappa = kappas[du.rank_simple(sum_cves)[0]]
+
 
