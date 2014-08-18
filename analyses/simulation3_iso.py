153340f39005c02693d4e6e5802f23d26358682b
Charles Yang Zheng <snarles@celadon>
2014-08-18 09:13:33 -0700

    isotropic comp testing

--- analyses/simulation3_iso.py
+++ analyses/simulation3_iso.py
@@ -1,6 +1,8 @@
+# tests isotropic vs anisotropic component
 # carries out CV kappa selection on subset of data
 
 import sys
+print sys.argv[0]
 barg = int(sys.argv[1])
 partarg = int(sys.argv[2])
 printarg = int(sys.argv[3])
@@ -9,36 +11,35 @@ import numpy as np
 import donuts.data as dnd
 import donuts.deconv.utils as du
 import dipy.data as dpd
-strname = "output_b"+str(barg)+"_part_"+str(partarg)+".npy"
 
 s1 = dpd.get_sphere('symmetric362')
 s2 = s1.subdivide() # s2 has 1442 vertices
 grid = s1.vertices
 kappas = np.arange(0.8,3,.05)
 
-data, bvecs0, bvals = dnd.load_hcp_cso()
+data, bvecs0, bvals = dnd.load_hcp_cc()
 bvecs0=bvecs0.T
 bval = barg
 idx = np.squeeze(np.nonzero(np.logical_and(bvals > bval-20, bvals < bval+20)))
 bvecs = bvecs0[idx,:]
 xss = du.build_xss(grid,bvecs,kappas)
+xss_1 = np.hstack(np.ones((n,1)),xss)
+xss_c = [xss[,v]-np.mean(xss[,v])]
 n= np.shape(bvecs)[0]
-
+    
 # real data
 
-sel_inds = du.fullfact([40,40])
-nvox = len(sel_inds)
+nvox = np.shape(data)[0]
 nits = 100
-inds = range((partarg-1)*nits,min(nvox,partarg*nits))
-all_cves = np.zeros((len(kappas),nits))
-for ii in range(nits):
+lrange = (partarg-1)*nits
+urange = min(partarg*nits,nvox)
+iis = range(lrange,urange)
+all_cves = np.zeros((len(kappas),len(iis)))
+strname = "s2cc_output_b"+str(barg)+"_part_"+str(partarg)+".npy"
+for ii in iis:
     if printarg==1:
-        print strname+" "+str(inds[ii])
-    y1 = data[sel_inds[inds[ii]][0],sel_inds[inds[ii]][1],0,idx]
+        print strname+" "+str(ii)
+    y1 = data[ii,idx]
     sel_kappa, cves = du.cv_sel_params(y1,xss,n,kappas)
-    all_cves[:,ii] = cves
-
+    all_cves[:,ii-lrange] = cves    
 np.save(strname,all_cves)
-
-    
-
