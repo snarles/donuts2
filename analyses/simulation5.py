8fdc505750b5261c81452a135adde398d76ebe56
Charles Yang Zheng <snarles@celadon>
2014-08-30 17:23:59 -0700

    multi-kernel

--- analyses/simulation5.py
+++ analyses/simulation5.py
@@ -4,12 +4,8 @@ import numpy as np
 import donuts.deconv.utils as du
 import dipy.data as dpd
 import donuts.data as dnd
-s1 = dpd.get_sphere('symmetric362')
-s2 = s1.subdivide() # s2 has 1442 vertices
+grid = du.sph_lattice(10,2)
 
-
-grid = s2.vertices
-kappas = np.arange(0.8,3,.05)
 
 #data, bvecs0, bvals = dnd.load_hcp_cso()
 data, bvecs0, bvals = dnd.load_hcp_cc()
@@ -17,35 +13,14 @@ bvecs0=bvecs0.T
 bval = 2000
 idx = np.squeeze(np.nonzero(np.logical_and(bvals > bval-20, bvals < bval+20)))
 bvecs = bvecs0[idx,:]
-xss = du.build_xss(grid,bvecs,kappas)
 n= np.shape(bvecs)[0]
+xs = du.ste_tan_kappa(grid, bvecs)
 
 # simulation
-nits = 100
-all_cves= np.zeros((len(kappas),nits))
-for ii in range(nits):
-    true_k = 2
-    true_pos = du.normalize_rows(np.random.normal(0,1,(true_k,3)))
-    true_w = np.ones((true_k,1))/true_k
-    true_kappa = 1.6
-    true_sigma = 0.1
-    y0, y1 = du.simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,true_sigma)
-    sel_kappa, cves = du.cv_sel_params(y1,xss,10,kappas)
-    all_cves[:,ii] = cves
-sum_cves = np.sum(all_cves,axis=1)
-sel_kappa = kappas[du.rank_simple(sum_cves)[0]]
-sel_xs = xss[du.rank_simple(sum_cves)[0]]
-tsses=[0.]*nits
-sses = [0.]*nits
-sumbs=[0.]*nits
-for ii in range(nits):
-    true_k = 3
-    true_pos = du.normalize_rows(np.random.normal(0,1,(true_k,3)))
-    true_w = np.ones((true_k,1))/true_k
-    y0, y1 = du.simulate_signal_kappa(np.sqrt(true_kappa)*true_pos,true_w,bvecs,true_sigma)
-    yh, beta, est_pos, est_w = du.ls_est(y1,sel_xs,grid)
-    sumbs[ii]=sum(beta)
-    sses[ii] = sum((yh-y1)**2)
-    tsses[ii]=sum((y1-y0)**2)
-tsigma_h = np.sqrt(np.mean(tsses)/n)
-sigma_h = np.sqrt(np.mean(sses)/n)
+true_k = 2
+true_pos = np.sqrt(true_kappa)*du.normalize_rows(np.random.normal(0,1,(true_k,3)))
+true_w = np.ones((true_k,1))/true_k
+true_kappa = 1.6
+true_sigma = 0.1
+y0, y1 = du.simulate_signal_kappa(true_pos,true_w,bvecs,true_sigma)
+yh, beta, est_pos, est_w = du.ls_est(y1,xs,grid)
