4d0b21996ddd0f6b058ede7355db79802a110d0c
Charles Yang Zheng <snarles@celadon>
2014-08-31 16:03:39 -0700

    rewriting simulation system

--- analyses/setup_simulations.py
+++ analyses/setup_simulations.py
@@ -8,5 +8,22 @@ f = open('datapath.txt','w')
 f.write(datapath)
 f.close()
 
+import numpy as np
+import donuts.data as dnd
+import donuts.deconv.utils as du
+import dipy.data as dpd
+
+data, bvecs0, bvals = dnd.load_hcp_cso2()
+bvecs0=bvecs0.T
+np.save(datapath + 'bvecs',bvecs0)
+np.save(datapath + 'bvals',bvals)
+np.save(datapath + 'cso',data)
+data, bvecs0, bvals = dnd.load_hcp_cc()
+np.save(datapath + 'cc',data)
+
+f = open(datapath + 'index.txt','w')
+f.write('[INDEX][DATA:cso:raw][DATA:cc:raw]')
+f.close()
+
 
 
