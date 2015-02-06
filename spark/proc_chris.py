e188869c0a28bb1a4a47738ae72cae5053c6b9cb
Charles Zheng EC2 <ubuntu@ip-172-31-24-174.us-west-2.compute.internal>
2015-02-06 22:56:50 +0000

    proc

--- spark/proc_chris.py
+++ spark/proc_chris.py
@@ -6,4 +6,24 @@ suffix = '_ec.nii.gz'
 import numpy as np
 import nibabel as nib
 fname = prefixes[dataset]+ str(ind) + suffix
+outname = savedirs[dataset] + 'coil' + str(ind)
 rawdata = nib.load(fname).get_data()
+rdr = rawdata.ravel()**2
+coords = np.vstack(np.unravel_index(range(120*120*69), (120, 120, 69))).T
+newdata = np.hstack([coords, ind * np.ones(993600, 1), np.array(rdr.reshape((993600, 150))*10000, dtype=int)])
+
+def int2str(z):
+    if (z < 120):
+        return chr(z)
+    else:
+        resid = int(z % 120)
+        z = int(z-resid)/120
+        return int2str(z)+chr(120)+chr(resid)
+    
+def ints2str(zs):
+    return ''.join(int2str(z) for z in zs)
+
+strs = [ints2str(nn) for nn in newdata]
+f = open(outname, 'w')
+f.write('\n'.join(strs))
+f.close()
