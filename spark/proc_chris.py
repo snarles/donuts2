1ada1cf8ff779d200867118370eff7e9407b62c4
Charles Zheng EC2 <ubuntu@ip-172-31-24-174.us-west-2.compute.internal>
2015-02-07 01:25:46 +0000

    encoding

--- spark/proc_chris.py
+++ spark/proc_chris.py
@@ -1,27 +1,41 @@
+def int2str(z):
+    if (z < 90):
+        return chr(z+33)
+    else:
+        resid = int(z % 90)
+        z = int(z-resid)/90
+        return int2str(z)+chr(90+33)+chr(resid+33)
+
+def ints2str(zs):
+    return ''.join(int2str(z) for z in zs)
+
+def str2ints(st):
+    os = [ord(c)-33 for c in st]
+    zs = []
+    counter = 0
+    while counter < len(os):
+        if os[counter] == 90:
+            zs[-1] = zs[-1] * 90 + os[counter + 1]
+            counter = counter + 1
+        else:
+            zs.append(os[counter])
+        counter = counter + 1
+    return zs
+
 sdata = (120, 120, 69, 150)
 prefixes = ['/home/ubuntu/predator/8631_5_1_pfile/coil_images/8631_5_coil', \
             '/home/ubuntu/predator/8631_11_1_pfile/coil_images/8631_11_coil']
-savedirs = ['/home/ubuntu/chris1/', '/home/ubuntu/chris2/']
+savedirs = ['/home/ubuntu/tempdata/', '/home/ubuntu/tempdata/']
 suffix = '_ec.nii.gz'
 import numpy as np
 import nibabel as nib
 fname = prefixes[dataset]+ str(ind) + suffix
-outname = savedirs[dataset] + 'coil' + str(ind)
+outname = savedirs[dataset] + 'coil' + str(ind) + '.cff'
+
 rawdata = nib.load(fname).get_data()
 rdr = rawdata.ravel()**2
 coords = np.vstack(np.unravel_index(range(120*120*69), (120, 120, 69))).T
 newdata = np.hstack([coords, ind * np.ones((993600, 1), dtype=int), np.array(rdr.reshape((993600, 150))*10000, dtype=int)])
-
-def int2str(z):
-    if (z < 120):
-        return chr(z)
-    else:
-        resid = int(z % 120)
-        z = int(z-resid)/120
-        return int2str(z)+chr(120)+chr(resid)
-    
-def ints2str(zs):
-    return ''.join(int2str(z) for z in zs)
 
 strs = [ints2str(nn) for nn in newdata]
 f = open(outname, 'w')
