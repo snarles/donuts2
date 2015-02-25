4937804cf1b995b56d756c95c5f475084ff8d5e6
snarles <snarles@stanford.edu>
2015-02-24 20:53:45 -0800

    no mds

--- convert.py
+++ convert.py
@@ -9,7 +9,6 @@ for dir in dirs:
     ipynbs = [file for file in files if '.ipynb' in file and '_checkpoints' not in file]
     for ipynb in ipynbs:
         subprocess.check_output('ipython nbconvert --to html '+ipynb, shell=True)
-        subprocess.check_output('ipython nbconvert --to markdown '+ipynb, shell=True)
     subprocess.check_output('git add *.ipynb *.html *.md', shell=True)
     os.chdir('..')
 subprocess.check_output('git commit -a -m "Auto HTML Conversion"', shell=True)
