5957b6f35b624d1c660a83e5be45c1b13bf7fd4f
snarles <snarles@stanford.edu>
2015-02-24 21:18:36 -0800

    pics

--- convert.py
+++ convert.py
@@ -9,7 +9,7 @@ for dir in dirs:
     ipynbs = [file for file in files if '.ipynb' in file and '_checkpoints' not in file]
     for ipynb in ipynbs:
         subprocess.check_output('ipython nbconvert --to html '+ipynb, shell=True)
-    subprocess.check_output('git add *.ipynb *.html *.md', shell=True)
+    subprocess.check_output('git add *.ipynb *.html', shell=True)
     os.chdir('..')
 subprocess.check_output('git commit -a -m "Auto HTML Conversion"', shell=True)
 
