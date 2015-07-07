b744d48341b7061f59e77ae69fee3d339f1a2e03
Charles Zheng <snarles@stanford.edu>
2015-07-06 21:21:16 -0700

    checking t/rt corr between residuals

--- misc/osmosis_stuff.R
+++ misc/osmosis_stuff.R
@@ -21,6 +21,12 @@ extract_vox <- function(V, vx) {
   ans
 }
 
+
+emds <- function(B1, B2, mc.cores = 3) {
+  unlist(mclapply(1:dim(B1)[2], function(i) arc_emd(pts, B1[-1, i], pts, B2[-1, i]), mc.cores = mc.cores))
+}
+
+
 #ddir <- function(s = "") paste0("/home/snarles/hcp/10s/115320/", s)
 ddir <- function(s = "") paste0("/home/snarles/predator/osmosis/", s)
 
@@ -47,27 +53,22 @@ bvecs2 <- (read.table(ddir("SUB1_b1000_2.bvecs"), header = FALSE, sep = "") %>%
 plot3d(bvecs1); points3d(1.01 * bvecs2, col = "red")
 
 ## ** nifti **
-
-
-
 (wm1 <- readNIfTI(ddir("SUB1_wm_mask.nii.gz")))
-(diff1 <- readNIfTI(ddir("SUB1_b1000_1.nii.gz")))
 (wm2 <- readNIfTI(ddir("SUB2_wm_mask.nii.gz")))
-
 
 dim(wm1) # 81 106 76
 dim(wm2) # 81 106 76
 
-gimage(wm1[, , 30])
-gimage(wm2[, , 30])
-
-gimage(wm1[, , 40])
-gimage(wm2[, , 40])
-
-## wm consensus
-wmc <- wm1 * wm2
-
-gimage(wmc[, , 40])
+# gimage(wm1[, , 30])
+# gimage(wm2[, , 30])
+# 
+# gimage(wm1[, , 40])
+# gimage(wm2[, , 40])
+# 
+# ## wm consensus
+# wmc <- wm1 * wm2
+# 
+# gimage(wmc[, , 40])
 
 ## smoothed WM (to find wm ROIs)
 
@@ -81,23 +82,108 @@ sum(wms > 1.99)
 roi_inds <- which(wms > 1.99, arr.ind = TRUE)
 plot3d(roi_inds, xlim = c(1, 81), ylim = c(1, 106), zlim = c(1, 76))
 
+nclust <- 20
+clust <- kmeans(roi_inds, nclust, nstart = 10)$cluster
+plot3d(0, 0, 0, xlim = c(1, 81), ylim = c(1, 106), zlim = c(1, 76))
+for (i in 1:nclust) {
+  points3d(roi_inds[clust == i, , drop = FALSE], col = rainbow(nclust)[i])
+}
+
+(diff1 <- readNIfTI(ddir("SUB1_b1000_1.nii.gz")))
 temp <- extract_vox(diff1, roi_inds)
 diff1r <- temp[, 11:160]
 so1r <- temp[, 1:10]
+rm(diff1); gc()
 
-kappa <- 1
+(diff2 <- readNIfTI(ddir("SUB2_b1000_1.nii.gz")))
+temp <- extract_vox(diff2, roi_inds)
+diff2r <- temp[, 11:160]
+so2r <- temp[, 1:10]
+rm(diff2); gc()
+
+kappa <- 3
 X1 <- cbind(1, stetan(bvecs1, pts, kappa))
-Y <- t(diff1r)#[, 1:10]
-B <- multi_nnls(X1, Y, mc.cores = 3)
-mu <- X1 %*% B
-resid <- Y - mu
+Y1 <- t(diff1r)#[, 1:10]
+B1 <- multi_nnls(X1, Y1, mc.cores = 3)
+mu1 <- X1 %*% B1
+resid1 <- Y1 - mu1
 
-dim(resid)
-res <- svd(resid)
-dim(res$u)
-plot(res$u[, 1])
+X2 <- cbind(1, stetan(bvecs2, pts, kappa))
+Y2 <- t(diff2r)#[, 1:10]
+B2 <- multi_nnls(X2, Y2, mc.cores = 3)
+mu2 <- X2 %*% B2
+resid2 <- Y2 - mu2
 
-cols <- hsv(h = 0.5, s = 1, v = 1:150/150)
 
-plot3d(bvecs1, col = cols[order(res$u[, 1])], size = 10)
-plot3d(bvecs1, col = cols[order(res$u[, 2])], size = 10)
+#dm <- as.matrix(dist(roi_inds))
+#dim(dm)
+
+# dim(resid)
+# res <- svd(resid)
+# dim(res$u)
+# plot(res$u[, 1])
+# 
+# cols <- hsv(h = 0.5, s = 1, v = 1:150/150)
+# 
+# plot3d(bvecs1, col = cols[order(res$u[, 1])], size = 10)
+# plot3d(bvecs1, col = cols[order(res$u[, 2])], size = 10)
+# 
+# 
+# plot3d(bvecs1, col = cols[order(mu[, 3])], size = 10)
+# 
+# 
+# plot3d(mu[, 3] * bvecs1, size = 5)
+# points3d(Y[, 3] * bvecs1, size = 5, col = "red")
+# 
+# min(resid[, 3])
+# plot3d((resid[, 3] + 80) * bvecs1, size = 5)
+
+
+
+
+####
+##  Look at the noise in a cluster
+####
+
+layout(1)
+
+layout(matrix(1:2, 1, 2))
+plot(svd(resid1)$d)
+plot(svd(resid2)$d)
+ii <- ii + 1
+for (ii in 1:nclust) {
+  plot(svd(resid1[, clust == ii])$d)
+  title(paste(ii))  
+  plot(svd(resid2[, clust == ii])$d)
+  title(paste(ii))  
+}
+
+
+
+plot3d(roi_inds, xlim = c(1, 81), ylim = c(1, 106), zlim = c(1, 76), col = "green")
+points3d(roi_inds[clust == 11, , drop = FALSE], size = 4, col = "black")
+
+plot(svd(resid1[, clust == 11])$u[, 1])
+plot(svd(resid2[, clust == 11])$u[, 1])
+
+layout(1)
+for (ii in 1:20) {
+  plot(svd(resid1[, clust == ii])$u[, 1], svd(resid2[, clust == ii])$u[, 1])
+  title(paste(ii))  
+}
+
+####
+##  Check prediction error of NNLS
+####
+
+n <- dim(roi_inds)[1]
+Yh1 <- X1 %*% B2
+Yh2 <- X2 %*% B1
+f2(Y1 - Yh1)/n ## 695383108
+f2(Y2 - Yh2)/n ## 695737087
+e_nnls <- emds(B1, B2, mc.cores = 3)
+mean(e_nnls) ## 0.385
+
+####
+## Apply ADMM 
+####
